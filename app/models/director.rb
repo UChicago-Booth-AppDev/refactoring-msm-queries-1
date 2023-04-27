14fdf22dc1f6f09d58fba2cff7655ba42629dc78
Guilherme Grisi <ggrisi@chicagobooth.edu>
2023-04-27 15:04:33 +0000

    refactoring msm queries _ first version

--- app/models/director.rb
+++ app/models/director.rb
@@ -11,4 +11,12 @@
 #  updated_at :datetime         not null
 #
 class Director < ApplicationRecord
+
+def filmography
+director_id = self.id
+matching_movies = Movie.where({ :director_id => director_id}).order({ :year => :asc})
+return matching_movies
+
+end 
+
 end
